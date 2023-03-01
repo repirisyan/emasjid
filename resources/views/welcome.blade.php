ef503196b95a1a4ad5ff289d7f1a635f9f4b3c31
repirisyan <repirisyan@icloud.com>
2023-03-01 10:17:45 +0700

    fix responsive carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,6 +42,15 @@
                 });
             $(".owl-carousel").owlCarousel({
                 lazyLoad: true,
+                nav: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    1000: {
+                        items: 3
+                    },
+                }
             });
         });
     </script>
